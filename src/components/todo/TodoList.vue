9ea43a81106d2d6e52932f528ec2f96752eb5c0f
dommrqz <jdominicm@hotmail.com>
2018-09-27 12:58:27 -0500

    Adding .env and other things

---
+++ src/components/todo/TodoList.vue
@@ -0,0 +1,26 @@
+<template>
+<ul class="list-gropu">
+      <li v-for="(task, index) in listTasks" v-bind:key="index" class="list-group-item">
+        {{ task.description }} - {{ task.isDone ?  'Completed' : 'Pending' }}
+        <button type="button" class="btn btn-sm btn-primary float-right" v-on:click="toggle(task)">
+          Toggle
+        </button>
+      </li>
+    </ul>
+</template>
+
+<script>
+    export default{
+        props: {
+            listTasks: {
+                type: Array,
+                required: true
+            }
+        },
+        methods:{
+        toggle(task) {
+            this.$emit('toggle', task)
+        }
+    }
+}
+</script>
